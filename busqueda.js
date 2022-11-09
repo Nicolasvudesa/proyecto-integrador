31a041d1b65e6f34639ba186261bbabb120c543d
simon10p <sdiezpena@udesa.edu.ar>
2022-11-09 18:07:30 -0300

    prompts terminados y busqueda avance

--- busqueda.js
+++ busqueda.js
@@ -1,5 +1,29 @@
+window.addEventListener("load", function(){
+    let query1 = location.search
+    let query2 = new URLSearchParams(query1)
+    let busqueda = query2.get(".search")
+
+    let resultado = this.document.querySelector(".resultados")
+    resultado.innerHTML += `Resultados para: ${busqueda}`
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=032a1cc5e170bb64ead032809385412a&language=en-US&query=${busqueda}&page=1`)
+	    .then(function(response){
+	    return response.json();
+})
+	    .then(function(data){
+	    console.log(data);
+
+        let resultadoBusqueda = document.querySelector(".resultados")
+        if(data.results.length == 0){
+            resultadoBusqueda.innerHTML += `La búsqueda ${busqueda} no dio ningún resultado`
+        }
+        // else{
+        //     for(let i = 0, )
+        // }
 
 
-            if(data.results.length==0){
-                resultadoBusqueda.innerHTML+="<h1>Lo sentimos no existen resultados </h1>"
-            }
+})
+	    .catch(function(error){
+	    console.log('El error es: ' + error);
+})
+})
---
+++ header.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(){
+    let busqueda = document.querySelector(".iBusqueda")
+    let buscar = document.querySelector(".search")
+    busqueda.addEventListener("submit", function(e){
+        e.preventDefault(e);
+        if(buscar.value == ""){
+            alert("Ingresa un termino para buscar")
+        }
+        else if(buscar.value.length<3){
+            alert("La busqueda debe contener al menos 3 caracteres")
+            }
+        else{
+            this.submit()
+        }
+    })
+})
+
