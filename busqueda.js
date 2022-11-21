5ad3f0c6357676a0fdbf1efc1344b160a2656668
Sicolas Shakalis <nicoshakalis@Sicolass-MacBook-Air.local>
2022-11-21 15:14:51 -0300

    botton mas resultados

--- busqueda.js
+++ busqueda.js
@@ -2,7 +2,8 @@ window.addEventListener("load", function(){
     //Capturamos la querystring
     let query1 = location.search
     let query2 = new URLSearchParams(query1)//me convierte la cadena de texto a objeto literal
-    let busqueda = query2.get("busqueda") 
+    let busqueda = query2.get("busqueda")
+    let nro_pagina = 1 
 
    
     let resultado = this.document.querySelector(".resultados")
@@ -25,7 +26,7 @@ window.addEventListener("load", function(){
                 resultadoBusqueda.innerText = `La búsqueda ${busqueda} no dio ningún resultado`
             }
             else{
-                for(let i = 0; i < data.results.length; i++){
+                for(let i = 0; i < 4; i++){
                     let path = data.results[i].poster_path
                     let img = `https://image.tmdb.org/t/p/w500/${path}`
                     let id = data.results[i].id
@@ -35,26 +36,61 @@ window.addEventListener("load", function(){
                     <p>${data.results[i].release_date}</p>
                 </article>`
                 }
+                container.innerHTML += `<button class="siguiente"> mas resultados> </button>`
             }
+            let siguiente = document.querySelector(".siguiente")
+            siguiente.addEventListener("click", function(){
+                console.log("click")
+                container.innerHTML = ""
+                nro_pagina += 1
+                fetch(`https://api.themoviedb.org/3/search/movie?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US&page=${nro_pagina}&include_adult=false&query=${busqueda}`)
+                .then(function(response){
+                    return response.json();
+                })
+                .then(function(data){
+                    console.log(data);
+                    if(data.results.length == 0){
+                        resultadoBusqueda.innerText = `La búsqueda ${busqueda} no dio ningún resultado`
+                    }
+                    else{
+                        for(let i = 0; i < 4; i ++){
+                            let path = data.results[i].poster_path
+                            let img = `https://image.tmdb.org/t/p/w500/${path}`
+                            let id = data.results[i].id
+                            container.innerHTML += `<article class = "pelicula_buscada">
+                            <a href="./detalle-pelis.html?movie_id=${id}"> <img src="${img}" alt="Foto${data.results[i].original_title} " class="foto-home"></a>
+                            <p class = "title">${data.results[i].original_title}</p>
+                            <p>${data.results[i].release_date}</p>
+                        </article>`   
+                        }
+
+
+                    }
+
+
+            }
+            )
+
+        })
+
         })
 	    .catch(function(error){
 	        console.log('El error es: ' + error);
         })
 
-        fetch(`https://api.themoviedb.org/3/search/tv?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US&page=1&include_adult=false&query=${busqueda}`)
+        fetch(`https://api.themoviedb.org/3/search/tv?api_key=2a3601e42fea0b8cec36fb4c1999c023&language=en-US&page=${nro_pagina}&include_adult=false&query=${busqueda}`)
 	    .then(function(response){
 	        return response.json();
         })
 	    .then(function(data){
             console.log(data);
-            let resultadoBusqueda = document.querySelector(".resultados");
             let series = document.querySelector(".resultados_series")
             let container_series = document.querySelector('.container_busqueda_series');
             if(data.results.length == 0){
                 series.innerHTML = ""
             }
             else{
-                for(let i = 0; i < 8; i++){
+                for(let i = 0; i < 4; i++){
                     let path = data.results[i].poster_path
                     let img = `https://image.tmdb.org/t/p/w500/${path}`
                     let id = data.results[i].id
@@ -64,19 +100,15 @@ window.addEventListener("load", function(){
                     <p>${data.results[i].first_air_date}</p>
                 </article>`
                 }
+                container_series.innerHTML += `<button class="siguiente"> mas resultados> </button>`
             }
+            
         })
+
+
 	    .catch(function(error){
 	        console.log('El error es: ' + error);
         })
+    
 })
-
-
-
-
-
-
-
-
-
 
